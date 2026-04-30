ba9208082196d59be7963361d31d239399422266
wahyuprasettya <wahyu.prasettya@gmail.com>
2026-04-30 12:06:50 +0700

    Updates

--- src/components/PageShell.jsx
+++
@@ -1,7 +0,0 @@
-export default function PageShell({ children, compact = false }) {
-  return (
-    <main className={`relative z-10 mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 ${compact ? 'py-16' : 'py-20 sm:py-24'}`}>
-      {children}
-    </main>
-  )
-}
